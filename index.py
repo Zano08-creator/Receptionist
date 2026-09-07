101001b075567e1febbb2986540bde9e1da703a8
Zano08-creator <zanolinalessandro@gmail.com>
2026-09-07 21:49:44 +0200

    Modify configuration for footer and image URL
    Updated the footer message and changed the image URL in the default configuration.

--- index.py
+++ index.py
@@ -43,8 +43,7 @@ DEFAULT_CONFIG = {
     "channel_id": None,
     "title": "📜 Le missioni della Gilda si sono rinnovate!",
     "description": (
-        "*Elyndra solleva lo sguardo dal registro delle missioni, "
-        "un sorriso gentile sulle labbra.*\n\n"
+        "\n\n"
         "Salve avventurieri! Le bacheche della **Gilda di Osteria** sono state "
         "appena aggiornate: le vecchie missioni sono state archiviate e "
         "**nuovi incarichi vi attendono** presso lo sportello.\n\n"
@@ -52,7 +51,7 @@ DEFAULT_CONFIG = {
     ),
     "footer": "Elyndra la Receptionist • Gilda di Osteria",
     "color": 0xB388FF,  # viola/lilla elfico
-    "image_url": None,  # se impostata, ha priorità sull'immagine locale
+    "image_url": "https://cdn.discordapp.com/attachments/1523058897714413698/1546608132682424340/26b49355-7328-4c44-95d6-9c0da7bf5b99_-_Modificata.png?ex=6aa06691&is=6a9f1511&hm=5d3a78984866185a73229380708d599fd8d1405b370c778a51dab78bceef3d36&";  # se impostata, ha priorità sull'immagine locale
 }
 
 
